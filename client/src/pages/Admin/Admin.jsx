1dd076c0a3c1a0c3d290a7fbb18639b52244cba2
Capde <capde22@gmail.com>
2023-11-16 19:15:20 -0300

    a

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef  } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,19 +9,32 @@ import ContractForm from '../../components/ContractForm/ContractForm';
 
 const Admin = () => {
 
+
+    const [formHeight, setFormHeight] = useState(0);
+    const [manageNFTsHeight, setManageNFTsHeight] = useState(0);
     const [showForm, setShowForm] = useState(false);
+    const [showManageNFTs, setShowManageNFTs] = useState(false);
+
     const formRef = useRef(null);
+    const manageRef = useRef(null);
 
     const handleApproveContract = () => {
+        setShowManageNFTs(false);
         setShowForm(true);
+        setManageNFTsHeight(0);
+        setFormHeight((prev) => (prev === 0 ? formRef.current.scrollHeight : 0));
+      };
+    
+      const handleManageNFTs = () => {
+        setShowForm(false);
+        setShowManageNFTs(true);
+        setFormHeight(0);
+        setManageNFTsHeight((prev) => (prev === 0 ? manageRef.current.scrollHeight : 0));
+      };
 
-        if (formRef.current) {
-            formRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-            });
-        }
-    };
+
+    //const formRef = useRef(null);
+    //const manageRef = useRef(null);
 
     return <div className='admin'>
         <Container className='margen'>
@@ -31,13 +44,19 @@ const Admin = () => {
             />
             <BigButton
                 text={'Manage NFTs'}
-            //onClickButton={() => showAlert()}
+                onClickButton={() => handleManageNFTs()}
             />
         </Container>
 
         {showForm && (
-            <Container className='margen'>
-                <ContractForm  ref={formRef} />
+            <Container className='margen' style={{ height: formHeight }}>
+                <ContractForm ref={formRef} />
+            </Container>
+        )}
+
+        {showManageNFTs && (
+            <Container className='margen' style={{ height: manageNFTsHeight }}>
+                <h4 ref={manageRef}>HOLA</h4>
             </Container>
         )}
 
