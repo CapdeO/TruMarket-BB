8359625134d0c78dfe2bf36340e39789e30e73eb
Genaro Molina <genamolina01@gmail.com>
2023-12-19 17:51:27 -0300

    Separando archivos

--- contracts/NFT1155.sol
+++ contracts/NFT1155.sol
@@ -1,131 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// ==========---------->>>>>   Libraries for FACTORY
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+
 // ==========---------->>>>>   Libraries for ERC1155
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-/**
- * @title Factory
- * @dev Contract that acts as a factory for creating financing contracts.
- * Uses OpenZeppelin for Pausable, Access Control, and UUPSUpgradeable functionalities.
- */
-contract Factory is
-    Initializable,
-    PausableUpgradeable,
-    AccessControlUpgradeable,
-    UUPSUpgradeable
-{
-    /* ========== STATE VARIABLES ========== */
-
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-
-    uint256 public contractsCounter; // Counter for created contracts
-    address[] private addressesERC1155Created; // Array that stores addresses of created contracts
-
-    /* ========== Events ========== */
-
-    event ContractCreated(address indexed deployedContract);
-
-    /* ========== Constructor and Initialization ========== */
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    /**
-     * @dev Initializes the contract by assigning roles and permissions to the admin.
-     */
-    function initialize() public initializer {
-        __Pausable_init();
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
-    }
-
-    /* ========== State Changing Functions ========== */
-
-    /**
-     * @dev Pauses the contract, can only be executed by the PAUSER_ROLE.
-     */
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    /**
-     * @dev Unpauses the contract, can only be executed by the PAUSER_ROLE.
-     */
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    /**
-     * @dev Internal function to authorize the upgrade, can only be executed by the UPGRADER_ROLE.
-     * @param newImplementation The new implementation address.
-     */
-    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
-
-    /**
-     * @dev Factory function that creates a new financing contract.
-     * @param _name Contract name.
-     * @param _operationAmount Total amount of the operation.
-     * @param _amountToFinance Amount to finance.
-     * @param _investmentFractions Number of fractions.
-     * @param _addUsdc Address of the USDT contract.
-     * @return The address of the newly created contract.
-     */
-    function FactoryFunc(
-        string memory _name,
-        uint256 _operationAmount,
-        uint256 _amountToFinance,
-        uint256 _investmentFractions,
-        address _addUsdc
-    ) public returns (address) {
-        contractsCounter += 1;
-
-        string memory _symbol = string(
-            abi.encodePacked("TM", Strings.toString(contractsCounter))
-        );
-
-        FinancingContract1155 newContract = new FinancingContract1155(
-            _name,
-            _symbol,
-            _operationAmount,
-            _amountToFinance,
-            _investmentFractions,
-            _addUsdc,
-            contractsCounter
-        );
-
-        address newContractAdd = address(newContract);
-        addressesERC1155Created.push(newContractAdd); // Store the new address in the array
-        emit ContractCreated(newContractAdd); // Emit the event
-        return newContractAdd; // Return the address of the new contract
-    }
-
-    /* ========== View Functions ========== */
-
-    /**
-     * @dev Returns the addresses of the created contracts.
-     * @return An array with the addresses of the created contracts.
-     */
-    function getAddresses() public view returns(address[] memory) {
-        return addressesERC1155Created;
-    }
-}
 
 
 interface IUSDT {
--- contracts/NftFactory.sol
+++ contracts/NftFactory.sol
@@ -7,13 +7,15 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-// ==========---------->>>>>   Librerias para el ERC721
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import { FinancingContract1155 } from "./NFT1155.sol";
 
-contract FactoryERC721 is
+
+/**
+ * @title Factory
+ * @dev Contract that acts as a factory for creating financing contracts.
+ * Uses OpenZeppelin for Pausable, Access Control, and UUPSUpgradeable functionalities.
+ */
+contract Factory is
     Initializable,
     PausableUpgradeable,
     AccessControlUpgradeable,
@@ -24,23 +26,23 @@ contract FactoryERC721 is
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
-    uint256 public contractsCounter;
-    address[] private addressesERC721Created;
-    mapping(address => uint256) public profits;
-
-    // Struct con financiacion, profits
+    uint256 public contractsCounter; // Counter for created contracts
+    address[] private addressesERC1155Created; // Array that stores addresses of created contracts
 
     /* ========== Events ========== */
 
-    event NewContractDeployed(address indexed deployedContract);
+    event ContractCreated(address indexed deployedContract);
 
-    /* ========== Constructor ========== */
+    /* ========== Constructor and Initialization ========== */
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
+    /**
+     * @dev Initializes the contract by assigning roles and permissions to the admin.
+     */
     function initialize() public initializer {
         __Pausable_init();
         __AccessControl_init();
@@ -51,23 +53,40 @@ contract FactoryERC721 is
         _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
-    /* ========== Change state ========== */
+    /* ========== State Changing Functions ========== */
 
+    /**
+     * @dev Pauses the contract, can only be executed by the PAUSER_ROLE.
+     */
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    /**
+     * @dev Unpauses the contract, can only be executed by the PAUSER_ROLE.
+     */
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyRole(UPGRADER_ROLE) {}
+    /**
+     * @dev Internal function to authorize the upgrade, can only be executed by the UPGRADER_ROLE.
+     * @param newImplementation The new implementation address.
+     */
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
 
-    // Minimal proxy --> clones de smart contracts menor costo EIP-1167
+    /**
+     * @dev Factory function that creates a new financing contract.
+     * @param _name Contract name.
+     * @param _operationAmount Total amount of the operation.
+     * @param _amountToFinance Amount to finance.
+     * @param _investmentFractions Number of fractions.
+     * @param _addUsdc Address of the USDT contract.
+     * @return The address of the newly created contract.
+     */
     function FactoryFunc(
         string memory _name,
+        uint256 _operationAmount,
         uint256 _amountToFinance,
         uint256 _investmentFractions,
         address _addUsdc
@@ -78,206 +97,29 @@ contract FactoryERC721 is
             abi.encodePacked("TM", Strings.toString(contractsCounter))
         );
 
-        FinancingContractERC721 newContract = new FinancingContractERC721(
+        FinancingContract1155 newContract = new FinancingContract1155(
             _name,
             _symbol,
+            _operationAmount,
             _amountToFinance,
             _investmentFractions,
-            _addUsdc
+            _addUsdc,
+            contractsCounter
         );
 
         address newContractAdd = address(newContract);
-
-        addressesERC721Created.push(newContractAdd);
-        emit NewContractDeployed(newContractAdd);
-        return newContractAdd;
+        addressesERC1155Created.push(newContractAdd); // Store the new address in the array
+        emit ContractCreated(newContractAdd); // Emit the event
+        return newContractAdd; // Return the address of the new contract
     }
 
-    /* ========== View functions ========== */
+    /* ========== View Functions ========== */
 
+    /**
+     * @dev Returns the addresses of the created contracts.
+     * @return An array with the addresses of the created contracts.
+     */
     function getAddresses() public view returns(address[] memory) {
-        return addressesERC721Created;
-    }
-}
-
-interface IUSDT {
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-
-    function decimals() external view returns (uint8);
-
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint256);
-
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function balanceOf(address account) external view returns (uint256);
-}
-
-contract FinancingContractERC721 is
-    ERC721,
-    ERC721Enumerable,
-    ERC721Pausable,
-    AccessControl
-{
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
-    uint256 private _nextTokenId;
-
-    uint256 public amountToFinance;
-    uint256 public investmentFractions;
-    uint256 public fractionPrice;
-    uint256 public buyBackPrice;
-    bool withdraw;
-    IUSDT usdt;
-
-    struct HistoryFractions {
-        uint256 fractions;
-        uint256 timestamp;
-        address owner;
-    }
-
-    HistoryFractions[] public historyFractions;
-
-    mapping (uint256 => address) investors;
-
-    enum Status {
-        OnSale,
-        Sold,
-        Milestones,
-        Finished
-    }
-    Status public contractStatus;
-
-    event Invest(address investor, uint256 fractions);
-    event TotalAmountFinanced();
-    event WithdrawComplete();
-    event newMilestone(uint256 step, string description);
-    event BurnNft(uint256 tokenId);
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _amountToFinance,
-        uint256 _investmentFractions,
-        address _addUsdt
-    ) ERC721(_name, _symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-
-        usdt = IUSDT(_addUsdt);
-
-        amountToFinance = _amountToFinance;
-        investmentFractions = _investmentFractions;
-        fractionPrice = amountToFinance / investmentFractions;
-    }
-
-    function buyFraction(uint256 _amount) public whenNotPaused {
-        uint256 priceInWei = (fractionPrice * (10**6)) * _amount;
-        require(contractStatus == Status.OnSale, "The sale is closed.");
-        require(_amount > 0, "Amount cannot be zero.");
-        require(_amount < investmentFractions + 1, "Amount to buy exceedes total fractions.");
-        require(usdt.balanceOf(msg.sender) >= priceInWei, 
-            "Inssuficient USDT balance.");
-        require(usdt.allowance(msg.sender, address(this)) >= priceInWei, 
-            "In order to proceed, you must approve the required amount of USDT.");
-        require(usdt.transferFrom(msg.sender, address(this), priceInWei), 
-            "USDT transfer error.");
-
-        // Ver ERC-1155 Mint Batch
-        for (uint8 i=0; i<_amount; i++) {
-            _safeMint(msg.sender, _nextTokenId);
-            _nextTokenId++;
-        }
-
-        emit Invest(msg.sender, _amount);
-
-        if (_nextTokenId == (investmentFractions + 1))
-            contractStatus = Status.Sold;
-
-        uint256 fractions = _amount;
-        uint256 timestamp = block.timestamp;
-        address owner = msg.sender;
-
-        historyFractions.push(HistoryFractions(fractions, timestamp, owner));
-    }
-
-    function setBuyBack(uint256 profit) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(profit > 0, "Profit can't be zero");
-
-        uint256 totalAmount = amountToFinance + ((amountToFinance * profit) / 100);
-
-        totalAmount = totalAmount * (10**6);
-
-        require(totalAmount <= usdt.balanceOf(msg.sender), "Not enough USDT balance");
-
-        require(usdt.allowance(msg.sender, address(this)) >= totalAmount, 
-            "In order to proceed, you must approve the required amount of USDT.");
-
-        usdt.transferFrom(msg.sender, address(this), totalAmount);
-
-        buyBackPrice = fractionPrice + ((fractionPrice * profit) / 100);
-
-        contractStatus = Status.Finished;
-    }
-
-    function withdrawBuyBack() public {
-        require(contractStatus == Status.Finished, "Contract is not finished.");
-        uint256 nftsAmount = balanceOf(msg.sender);
-
-        require(nftsAmount > 0, "Caller has not tokens.");
-
-        uint256 totalAmount = nftsAmount * buyBackPrice;
-
-        // SIN BUCLE ????
-        for (uint256 i = 0; i < nftsAmount; i++) {
-            _burn(tokenOfOwnerByIndex(msg.sender, i));
-        }
-
-        usdt.transfer(msg.sender, totalAmount);
-    }
-
-    function withdrawUSDT() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        uint256 contractBalance = usdt.balanceOf(address(this));
-        require(contractStatus == Status.Sold, "Not on sold status.");
-        require(usdt.transfer(msg.sender, contractBalance), "USDT transfer error.");
-        contractStatus = Status.Milestones;
-    }
-
-    function getHistorial() public view returns (HistoryFractions[] memory) {
-        return historyFractions;
-    }
-
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable, ERC721Pausable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+        return addressesERC1155Created;
     }
 }
